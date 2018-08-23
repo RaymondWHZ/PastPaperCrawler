1c7ee10666648cefa9e08017582e48b9c29238b4
Haozhen Wu <wuhaozhen1106@gmail.com>
2018-08-23 21:03:19 +0800

    Updated core

--- crawler_core.py
+++ crawler_core.py
@@ -7,11 +7,6 @@ forge_agent_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0
 
 
 def search_with_specified_url(specified_url):
-    pass
-
-
-def search_all(subject, code):
-    specified_url = gce_guide_root_url + subject + "%20(" + code + ")/"
     i_rq = rq.Request(url=specified_url, headers=forge_agent_header)
     ret = rq.urlopen(i_rq).read()
     html_phrase = BeautifulSoup(ret, 'lxml')
@@ -20,23 +15,29 @@ def search_all(subject, code):
 
     all_papers = []
     for p in papers:
-        print(papers)
         all_papers.append(p['href'])
 
     return all_papers
 
 
+def search_all(subject, code):
+    specified_url = gce_guide_root_url + subject + "%20(" + code + ")/"
+    return search_with_specified_url(specified_url)
+
+
 def download_all(subject, code, to_path):
-    for url in search_all(subject, code):
-        i_rq = rq.Request(url=url, headers=forge_agent_header)
+    specified_url = gce_guide_root_url + subject + "%20(" + code + ")/"
+
+    for paper in search_with_specified_url(specified_url):
+        i_rq = rq.Request(url=specified_url+paper, headers=forge_agent_header)
         ret = rq.urlopen(i_rq).read()
-        with open(to_path + url[url.rfind("/")+1:], "wb") as f:
+        with open(to_path + paper, "wb") as f:
             f.write(ret)
 
 
 def main():
     print(search_all("Economics", "0455"))
-    download_all("Economics", "0455", "/User/wuhaozhen/Downloads/Economics/")
+    download_all("Economics", "0455", "/Users/wuhaozhen/Downloads/Economics/")
 
 
 if __name__ == '__main__':
