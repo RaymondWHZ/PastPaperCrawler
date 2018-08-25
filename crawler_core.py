b7c61d1131b09482054d4da11a430d3b030fde95
Haozhen Wu <wuhaozhen1106@gmail.com>
2018-08-25 23:43:18 +0800

    Reconstruct gui and made small change in core

--- crawler_core.py
+++ crawler_core.py
@@ -3,9 +3,7 @@ from bs4 import BeautifulSoup
 import ssl
 import re
 
-
 ssl._create_default_https_context = ssl._create_unverified_context
-
 
 gce_guide_root_url = "https://papers.gceguide.com/IGCSE/"
 forge_agent_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
@@ -22,7 +20,10 @@ def __search_with_specified_url(specified_url):
 
     paper_icons = html_phrase.select('#ggTable > tbody > tr > td > a')
     for paper_icon in paper_icons:
-        yield paper_icon['href']
+        paper = paper_icon['href']
+        if not re.findall(r'\d{4}_[swy]\d{2}_\w{2}_[1234][123].pdf', paper):
+            continue
+        yield paper
 
 
 def search_all(subject, code):
@@ -35,10 +36,6 @@ def download_all(subject, code, to_path):
     specified_url = __gce_guide_specified_link(subject, code)
 
     for paper in __search_with_specified_url(specified_url):
-        print(paper)
-        if not re.findall(r'\d{4}_[swy]\d{2}_\w{2}_[1234][123].pdf', paper):
-            continue
-
         i_rq = rq.Request(url=specified_url+paper, headers=forge_agent_header)
         ret = rq.urlopen(i_rq).read()
 
--- gui.py
+++ gui.py
@@ -1,42 +1,54 @@
 from crawler_core import *
 from tkinter import *
-from tkinter.filedialog import *
+from tkinter.filedialog import askdirectory
 
-def ui_make_app():
-    app = Tk()
-    app.geometry('1920x1080')
 
-    sub_selection = StringVar(app)
-    all_subjects_name = ['Economics_0455', 'Biology_0610']
+class MainFrame(Tk):
 
-    OptionMenu(app, sub_selection, *all_subjects_name).grid(row=1, column=1)
-    Button(app, text='Search', command=lambda: ui_update_papers(sub_selection.get())).grid(row=1, column=2)
+    def __init__(self):
+        super().__init__()
 
-    Listbox(app, name='paper_shower', width=50, selectmode=MULTIPLE).grid(row=2, column=1)
-    Button(app, text='Download', command=lambda: download_all(
-        sub_selection.get().split('_')[0], sub_selection.get().split('_')[1], askdirectory()+'/'))\
-        .grid(row=2, column=2)
+        self.geometry('1920x1080')  # reset size
 
-    return app
+        self.sub_selection = sub_selection = StringVar(self)  # variable that records option menu selection
+        all_subjects_name = ['Economics_0455', 'Biology_0610']  # available choices
 
-def ui_update_papers(paper_str):
-    code = paper_str.split('_')[1]
-    name = paper_str.split('_')[0]
+        # subject option box
+        OptionMenu(self, sub_selection, *all_subjects_name).grid(row=1, column=1)
 
-    all_papers = search_all(name, code)
+        # search button
+        Button(self, text='Search', command=self.update_paper_list).grid(row=1, column=2)
 
-    ps = app.children['paper_shower']
-    ps.delete(0, END)
+        # paper list
+        Listbox(self, name='paper_shower', width=50, selectmode=MULTIPLE).grid(row=2, column=1)
 
-    for paper in all_papers:
-        ps.insert(END, paper)
+        # download button
+        Button(self, text='Download', command=self.download_all).grid(row=2, column=2)
 
-    print(all_papers)
+    def update_paper_list(self):
+        paper_str = self.sub_selection.get()
+        subject = paper_str.split('_')[0]
+        code = paper_str.split('_')[1]
+
+        all_papers = search_all(subject, code)
+
+        ps = self.children['paper_shower']
+        ps.delete(0, END)
+
+        for paper in all_papers:
+            ps.insert(END, paper)
+
+    def download_all(self):
+        paper_str = self.sub_selection.get()
+        subject = paper_str.split('_')[0]
+        code = paper_str.split('_')[1]
+
+        target_dir = askdirectory() + "/"
+        download_all(subject, code, target_dir)
 
 
 def main():
-    global app
-    app = ui_make_app()
+    app = MainFrame()
     app.mainloop()
 
 
