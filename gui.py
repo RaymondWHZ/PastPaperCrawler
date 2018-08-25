22939d77906e302a509ce511ced646f9757ae2ef
liuxingjian <1172413773@qq.com>
2018-08-25 17:01:43 +0800

    Allows the user to download only the papers selected.

--- gui.py
+++ gui.py
@@ -2,6 +2,12 @@ from crawler_core import *
 from tkinter import *
 from tkinter.filedialog import *
 
+info = {
+    'database_location': 'Please select the folder-->',
+
+}
+
+
 def ui_make_app():
     '''
     Function that initialized the GUI interface.
@@ -14,16 +20,50 @@ def ui_make_app():
     all_subjects_name = ['Economics_0455', 'Biology_0610']
     # Subject name in format <subject>_<code>
 
+    # Row 1
     OptionMenu(app, sub_selection, *all_subjects_name).grid(row=1, column=1)
     Button(app, text='Search', command=lambda: ui_update_papers(sub_selection.get())).grid(row=1, column=2)
 
-    Listbox(app, name='paper_shower', width=50, selectmode=MULTIPLE).grid(row=2, column=1)
-    Button(app, text='Download', command=lambda: download_all(
-        sub_selection.get().split('_')[0], sub_selection.get().split('_')[1], askdirectory()+'/')
+    # Row 2
+
+    # Row 3
+    Label(app, text='Database Location:'+info['database_location'], name='database_loc').grid(row=3, column=1, sticky=W)
+    Button(app, text='...', command=__update_database_location).grid(row=3, column=2)
+
+    # Row 4
+
+    # Row 5
+    Listbox(app, name='paper_shower', width=50, selectmode=MULTIPLE).grid(row=5, column=1, rowspan=3)
+    Button(app, text='Download All', state=DISABLED, command=lambda: download_all(
+        sub_selection.get().split('_')[0], sub_selection.get().split('_')[1], info['database_location']+'/')
            )\
-        .grid(row=2, column=2)
+        .grid(row=5, column=2)
+
+    # Row 6
+    Button(app, text='Download Selected',  command=lambda: download_papers(
+        sub_selection.get().split('_')[0], sub_selection.get().split('_')[1], info['database_location'] + '/', app.children['paper_shower'])
+           ).grid(row=6, column=2)
+
+
 
     return app
+
+
+def __update_database_location():
+    '''
+    Function which updates the download location.
+    :return: <None>
+    '''
+    new_location = askdirectory()
+
+    info['database_location'] = new_location
+    app.children['database_loc']['text'] = 'Database Location:' + info['database_location']
+
+
+
+
+
+
 
 def ui_update_papers(paper_str):
     '''
