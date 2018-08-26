3508af8607c84e51989ecf390eb587a0d468a9be
Haozhen Wu <wuhaozhen1106@gmail.com>
2018-08-26 10:12:52 +0800

    small changes

--- crawler_core.py
+++ crawler_core.py
@@ -3,9 +3,7 @@ from bs4 import BeautifulSoup
 import ssl
 import re
 
-
 ssl._create_default_https_context = ssl._create_unverified_context
-
 
 gce_guide_root_url = "https://papers.gceguide.com/IGCSE/"
 forge_agent_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
@@ -34,7 +32,7 @@ def search_all(subject, code):
     return list(__search_with_specified_url(specified_url))
 
 
-def download_paper(specified_url, paper, to_path):
+def __download_paper(specified_url, paper, to_path):
     i_rq = rq.Request(url=specified_url + paper, headers=forge_agent_header)
     ret = rq.urlopen(i_rq).read()
 
@@ -43,31 +41,24 @@ def download_paper(specified_url, paper, to_path):
         f.write(ret)
 
 
-def download_all(subject, code, to_path):
-    specified_url = __gce_guide_specified_link(subject, code)
-
-    for paper in __search_with_specified_url(specified_url):
-        download_paper(specified_url, paper, to_path)
-
-
-def download_papers(subject, code, to_path, lb):
+def download_papers(subject, code, to_path, specified_papers=None):
     """
     Function that downloads papers only selected by the user.
     :param subject: <str> The subject to download. Obtained by OptionMenu Selection.
     :param code: <str> The code of the subject to download. Obtained by OptionMenu Selection
     :param to_path: <str> The download path.
-    :param lb: <ListBox> The listbox which the user does his selection.
+    :param specified_papers: <list> The specified papers to be downloaded
     :return: <None>
     """
-    paper_pos = lb.curselection()
-    paper_name = []
-    for pp in paper_pos:
-        paper_name.append(lb.get(pp))
 
     specified_url = __gce_guide_specified_link(subject, code)
 
-    for pn in paper_name:
-        download_paper(specified_url, pn, to_path)
+    if specified_papers is None:
+        specified_papers = __search_with_specified_url(specified_url)
+
+    for paper in specified_papers:
+        print(paper)
+        __download_paper(specified_url, paper, to_path)
 
 
 def main():
--- gui.py
+++ gui.py
@@ -6,105 +6,147 @@ info = {
     'database_location': 'Please select the folder-->',
 }
 
-
-def ui_make_app():
-    """
-    Function that initialized the GUI interface.
-    :return: <Tk> The GUI Interface
-    """
-    app = Tk()
-    app.geometry('1920x1080')
-
-    sub_selection = StringVar(app)
-    all_subjects_name = ['Economics_0455', 'Biology_0610', 'Geography_0460', 'History_0470', 'Mathematics_0580',
-                         'Physics_0625', 'English - Second Language (oral endorsement)_0510', 'Computer Science_0478',
-                         'Chemistry_0620', 'Drama_0411', 'Business Studies_0450']
-    # Subject name in format <subject>_<code>
-
-    # Row 1
-    OptionMenu(app, sub_selection, *all_subjects_name).grid(row=1, column=1)
-    Button(app, text='Search', command=lambda: (ui_update_papers(sub_selection.get()), ui_update_button())).grid(row=1, column=2)
-
-    # Row 2
-
-    # Row 3
-    Label(app, text='Database Location:'+info['database_location'], name='database_loc').grid(row=3, column=1, sticky=W)
-    Button(app, text='...', name='bt_ask_db_loc', command=lambda: (__update_database_location(), ui_update_button())).grid(row=3, column=2)
-
-    # Row 4
-
-    # Row 5
-    Listbox(app, name='paper_shower', width=50, selectmode=MULTIPLE).grid(row=5, column=1, rowspan=3)
-    Button(app, text='Download All', state=DISABLED, name='bt_download_all', command=lambda: download_all(
-        sub_selection.get().split('_')[0], sub_selection.get().split('_')[1], info['database_location']+'/')
-           )\
-        .grid(row=5, column=2)
-
-    # Row 6
-    Button(app, text='Download Selected', state=DISABLED, name='bt_download_selected', command=lambda: download_papers(
-        sub_selection.get().split('_')[0], sub_selection.get().split('_')[1], info['database_location'] + '/', app.children['paper_shower'])
-           ).grid(row=6, column=2)
-
-    return app
+# Subject name in format <subject>_<code>
+all_subjects_names = [
+    'Economics_0455', 'Biology_0610', 'Geography_0460', 'History_0470', 'Mathematics_0580',
+    'Physics_0625', 'English - Second Language (oral endorsement)_0510',
+    'Computer Science_0478', 'Chemistry_0620', 'Drama_0411', 'Business Studies_0450'
+]
 
 
-def __update_database_location():
-    """
-    Function which updates the download location.
-    :return: <None>
-    """
-    new_location = askdirectory()
+class MainWindow(Tk):
 
-    info['database_location'] = new_location
-    app.children['database_loc']['text'] = 'Database Location:' + info['database_location']
+    def __init__(self):
+        super().__init__()
 
+        self.geometry('1920x1080')
 
-def ui_update_papers(paper_str):
-    """
-    Function that shows the past papers onto the list box.
-    :param paper_str: <str> String contains subject and code from listbox, e.g. Economics_0455
-    :return: <None>
-    """
+        # variable to keep option menu selection
+        self.subject_selection = subject_selection = StringVar(self)
 
-    subject = paper_str.split('_')[0]
-    code = paper_str.split('_')[1]
+        # Row 1
 
-    all_papers = search_all(subject, code)
+        # subject option menu
+        OptionMenu(
+            self, subject_selection, *all_subjects_names
+        ).grid(row=1, column=1)
 
-    ps = app.children['paper_shower']
-    ps.delete(0, END)   # Clear the listbox
+        # search all button
+        Button(
+            self, text='Search', command=self.search_button_clicked
+        ).grid(row=1, column=2)
 
-    for paper in all_papers:
-        ps.insert(END, paper)
+        # Row 2
 
+        # Row 3
+        Label(
+            self, text='Database Location:' + info['database_location'], name='database_loc'
+        ).grid(row=3, column=1, sticky=W)
 
-def ui_update_button():
-    """
-    Function that updates the button status. If an database location is given then the download buttons are enabled.
-    :return: <None>
-    """
+        Button(
+            self, text='...', name='bt_ask_db_loc', command=self.database_loc_button_clicked
+        ).grid(row=3, column=2)
 
-    database_bt = app.children['bt_ask_db_loc']
-    all_bt = app.children['bt_download_all']
-    selected_bt = app.children['bt_download_selected']
+        # Row 4
 
-    if info['database_location'] == 'Please select the folder-->':
-        database_bt.flash()
-        all_bt['state'] = DISABLED
-        selected_bt['state'] = DISABLED
+        # Row 5
+        Listbox(
+            self, name='paper_shower', width=50, selectmode=MULTIPLE
+        ).grid(row=5, column=1, rowspan=3)
 
-    else:
-        all_bt['state'] = ACTIVE
-        selected_bt['state'] = ACTIVE
+        Button(
+            self, text='Download All', state=DISABLED, name='bt_download_all',
+            command=self.download_all_button_clicked
+        ).grid(row=5, column=2)
+
+        # Row 6
+        Button(
+            self, text='Download Selected', state=DISABLED, name='bt_download_selected',
+            command=self.download_selected_button_clicked
+        ).grid(row=6, column=2)
+
+    def __update_database_location(self):
+        """
+        Function which updates the download location.
+        :return: <None>
+        """
+
+        new_location = askdirectory()
+
+        info['database_location'] = new_location
+        self.children['database_loc']['text'] = 'Database Location:' + new_location
+
+    def __update_buttons(self):
+        """
+        Function that updates the button status. If an database location is given then the download buttons are enabled.
+        :return: <None>
+        """
+
+        database_bt = self.children['bt_ask_db_loc']
+        all_bt = self.children['bt_download_all']
+        selected_bt = self.children['bt_download_selected']
+
+        if info['database_location'] == 'Please select the folder-->':
+            database_bt.flash()
+            all_bt['state'] = DISABLED
+            selected_bt['state'] = DISABLED
+        else:
+            all_bt['state'] = ACTIVE
+            selected_bt['state'] = ACTIVE
+
+    def __update_paper_list(self):
+        """
+        Function that shows the past papers onto the list box.
+        :param paper_str: <str> String contains subject and code from listbox, e.g. Economics_0455
+        :return: <None>
+        """
+
+        paper_str = self.subject_selection.get()
+        subject = paper_str.split('_')[0]
+        code = paper_str.split('_')[1]
+
+        all_papers = search_all(subject, code)
+
+        ps = self.children['paper_shower']
+        ps.delete(0, END)  # Clear the listbox
+
+        for paper in all_papers:
+            ps.insert(END, paper)
+
+    def search_button_clicked(self):
+        self.__update_paper_list()
+        self.__update_buttons()
+
+    def database_loc_button_clicked(self):
+        self.__update_database_location()
+        self.__update_buttons()
+
+    def download_all_button_clicked(self):
+        paper_str = self.subject_selection.get()
+        subject = paper_str.split('_')[0]
+        code = paper_str.split('_')[1]
+        to_path = info['database_location'] + '/'
+
+        download_papers(subject, code, to_path)
+
+    def download_selected_button_clicked(self):
+        paper_str = self.subject_selection.get()
+        subject = paper_str.split('_')[0]
+        code = paper_str.split('_')[1]
+        to_path = info['database_location'] + '/'
+
+        paper_shower = self.children['paper_shower']
+        selected_indices = paper_shower.curselection()
+
+        def get_specified_papers():
+            for index in selected_indices:
+                yield paper_shower.get(index)
+
+        download_papers(subject, code, to_path, get_specified_papers())
 
 
 def main():
-    """
-    MAIN.
-    :return: <None>
-    """
-    global app
-    app = ui_make_app()
+    app = MainWindow()
     app.mainloop()
 
 
