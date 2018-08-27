3e3daf607df90299452cdef3e7faf2cc22631df2
Haozhen Wu <wuhaozhen1106@gmail.com>
2018-08-27 20:39:05 +0800

    improved class PastPaperWedsite

--- crawler_core.py
+++ crawler_core.py
@@ -1,64 +1,26 @@
 import urllib.request as rq
-from bs4 import BeautifulSoup
 import ssl
-import re
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-gce_guide_root_url = "https://papers.gceguide.com/IGCSE/"
 forge_agent_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
 
 
-def __gce_guide_specified_link(subject, code):
-    return gce_guide_root_url + subject + "%20(" + code + ")/"
+def read_url(url):
+    i_rq = rq.Request(url=url, headers=forge_agent_header)
+    return rq.urlopen(i_rq).read()
 
 
-def __search_with_specified_url(specified_url):
-    i_rq = rq.Request(url=specified_url, headers=forge_agent_header)
-    ret = rq.urlopen(i_rq).read()
-    html_phrase = BeautifulSoup(ret, 'lxml')
+def download_file(url: str, to_path: str):
+    if to_path.endswith('/'):
+        to_path += '/'
 
-    paper_icons = html_phrase.select('#ggTable > tbody > tr > td > a')
-    for paper_icon in paper_icons:
-        paper = paper_icon['href']
-        if not re.findall(r'\d{4}_[swy]\d{2}_\w{2}_[1234][123].pdf', paper):
-            continue
-        yield paper
-
-
-def search_all(subject, code):
-    specified_url = __gce_guide_specified_link(subject, code)
-
-    return list(__search_with_specified_url(specified_url))
-
-
-def __download_paper(specified_url, paper, to_path):
-    i_rq = rq.Request(url=specified_url + paper, headers=forge_agent_header)
+    i_rq = rq.Request(url=url, headers=forge_agent_header)
     ret = rq.urlopen(i_rq).read()
 
-    with open(to_path + paper, "wb") as f:
-        print('Downloading:', paper)
+    file_name = url[url.rfind('/') + 1:]
+    with open(to_path + file_name, "wb") as f:
         f.write(ret)
-
-
-def download_papers(subject, code, to_path, specified_papers=None):
-    """
-    Function that downloads papers only selected by the user.
-    :param subject: <str> The subject to download. Obtained by OptionMenu Selection.
-    :param code: <str> The code of the subject to download. Obtained by OptionMenu Selection
-    :param to_path: <str> The download path.
-    :param specified_papers: <list> The specified papers to be downloaded
-    :return: <None>
-    """
-
-    specified_url = __gce_guide_specified_link(subject, code)
-
-    if specified_papers is None:
-        specified_papers = __search_with_specified_url(specified_url)
-
-    for paper in specified_papers:
-        print(paper)
-        __download_paper(specified_url, paper, to_path)
 
 
 def main():
--- ppwebsite.py
+++ ppwebsite.py
@@ -1,26 +1,73 @@
+import crawler_core as cc
+from bs4 import BeautifulSoup
+import re
+
+
+def is_paper(name: str):
+    return len(re.findall(r'\d{4}_[swy]\d{2}_\w{2}_[1234][123].pdf', name)) == 1
+
+
 class Subject(object):
-    name: str
-    code: str
+
+    def __init__(self, name: str, code: str=None):
+        """
+        Call it in either way:
+
+        1. Subject("Physics (0625)")
+        2. Subject("Physics", "0625")
+
+        Use str(subject) to get "Physics (0625)"
+        """
+        if code is None:
+            sep = name.rfind('(')
+            self.name = name[:sep - 1]
+            self.code = name[sep + 1:sep + 5]
+        else:
+            self.name = name
+            self.code = code
+
+    def __str__(self):
+        return self.name + " (" + self.code + ")"
 
 
-class PastPaperWebsite:
+class PastPaperWebsite(object):
 
     def __init__(self):
         self.__root_url = "https://"
-        self.__level_subject_site = {
+        self.__level_site = {
             "IGCSE": "",
             "AS & A-Level": "",
             "O Level": ""
         }
 
-    def get_levels(self):
+    def get_subjects(self, level: str) -> iter:
+        """
+        Search for subjects.
+
+        :param level: The level of the subject to search, one among "IGCSE", "AS & A-Level", "O Level".
+        :return: Iterator that returns all subjects.
+        """
         pass
 
-    def get_subjects(self, level):
+    def get_papers(self, level: str, subject: Subject) -> iter:
+        """
+        Search for all papers.
+
+        :param level: The level of the subject to search, one among "IGCSE", "AS & A-Level", "O Level".
+        :param subject: The subject to search.
+        :return: Iterator that returns all papers.
+        """
         pass
 
-    def get_papers(self, level, subject, season=None, year=None,
-                   cata=None, paper_num=None, edition_num=None):
+    def download_papers(self, level: str, subject: Subject, specified_papers: list, to_path: str):
+        """
+        Function that downloads paper.
+
+        :param level: The level of the subject to download, one among "IGCSE", "AS & A-Level", "O Level".
+        :param subject: The subject to download.
+        :param specified_papers: The specified papers to be downloaded
+        :param to_path: The download path.
+        """
         pass
 
 
@@ -29,11 +76,39 @@ class GCEGuide(PastPaperWebsite):
     def __init__(self):
         super().__init__()
         self.__root_url = "https://papers.gceguide.com/"
-        self.__level_subject_site = {
+        self.__level_site = {
             "IGCSE": "IGCSE/",
             "AS & A-Level": "A%20Levels/",
             "O Level": "O%20Levels/"
         }
+
+    @staticmethod
+    def __subject_site(subject):
+        return subject.name + "%20(" + subject.code + ")/"
+
+    @staticmethod
+    def __get_content_list(specified_url, criteria=lambda name: True):
+        ret = cc.read_url(specified_url)
+        html_phrase = BeautifulSoup(ret, 'lxml')
+
+        file_icons = html_phrase.select('#ggTable > tbody > tr > td > a')
+        for file_icon in file_icons:
+            file = file_icon['href']
+            if criteria(file):
+                yield file
+
+    def get_subjects(self, level):
+        specified_url = self.__root_url + self.__level_site[level]
+        return self.__get_content_list(specified_url, lambda name: name != "error_log")
+
+    def get_papers(self, level, subject):
+        specified_url = self.__root_url + self.__level_site[level] + self.__subject_site(subject)
+        return self.__get_content_list(specified_url, is_paper)
+
+    def download_papers(self, level, subject, specified_papers, to_path):
+        specified_url = self.__root_url + self.__level_site[level] + self.__subject_site(subject)
+        for paper in specified_papers:
+            cc.download_file(specified_url + paper, to_path)
 
 
 class PapaCambridge(PastPaperWebsite):
@@ -46,3 +121,12 @@ class PapaCambridge(PastPaperWebsite):
             "AS & A-Level": "AS%20and%20A%20Level/",
             "O Level": "GCE%20International%20O%20Level/"
         }
+
+
+def main():
+    gce_guide = GCEGuide()
+    gce_guide.download_papers("IGCSE", Subject("Physics (0625)"), ['0625_s14_ms_11.pdf', '0625_s14_ms_12.pdf', '0625_s14_ms_13.pdf'], '/Users/wuhaozhen/Downloads/Economics/')
+
+
+if __name__ == '__main__':
+    main()
