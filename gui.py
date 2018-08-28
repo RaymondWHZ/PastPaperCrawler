b2aabf15b68901a73bacdb15071db8e25ec79f0e
liuxingjian <1172413773@qq.com>
2018-08-28 19:52:28 +0800

    Supports Query.

--- gui.py
+++ gui.py
@@ -1,9 +1,21 @@
 from crawler_core import *
 from tkinter import *
 from tkinter.filedialog import askdirectory
+from ppwebsite import *
+
+import multiprocessing as mul
 
 info = {
     'database_location': 'Please select the folder-->',
+}
+
+
+criteria = {
+    'year': [],
+    'season': [],
+    'index': [],
+    'type': []
+
 }
 
 # Subject name in format <subject>_<code>
@@ -14,12 +26,83 @@ all_subjects_names = [
 ]
 
 
+using_website = GCEGuide()
+
+
+def get_years(all_papers):
+    all_years = []
+
+    for paper in all_papers:
+        if paper[6:8] not in all_years:
+            all_years.append(paper[6:8])
+
+    return all_years
+
+
+class ListBoxPlus(Listbox):
+    def __init__(self, master=None, cnf={}, **kw):
+        self.contents = list()
+
+        super().__init__(master, cnf, **kw)
+
+    def insert(self, index, *elements):
+        print(index, *elements)
+        for e in elements:
+            self.contents.append(e)
+
+        super().insert(index, *elements)
+
+    def delete(self, first, last=None):
+        last_pos = len(self.contents) if last == END else last
+
+        for count in range(first, last_pos):
+            self.contents[count] = 'Deleting'
+
+        count = 0
+        while count < len(self.contents):
+            if self.contents[count] == 'Deleting':
+                self.contents.pop(count)
+            else:
+                count += 1
+
+        super().delete(first, last)
+
+    def clear(self):
+        super().delete(0, END)
+
+    def query(self, year=None, paper=None, type=None):
+        self.clear()
+
+        for p in self.contents:
+
+            print(self.contents)
+
+            if p is None:
+                continue
+
+            if year is not None and len(year) != 0:
+                if p[5:8] not in year:
+                    continue
+
+            if paper is not None and len(paper) != 0:
+                if p[12:14] not in paper:
+                    continue
+
+            if type is not None and len(type)!= 0:
+                if p[9:11] not in type:
+                    continue
+
+            super().insert(END, p)
+
+
+
+
 class MainWindow(Tk):
 
     def __init__(self):
         super().__init__()
 
-        self.geometry('1920x1080')
+        self.geometry('720x1080')
 
         self.__subject_selection = StringVar(self)  # variable to keep option menu selection
         self.__shown_papers = []  # variable to keep all shown papers in listbox
@@ -50,7 +133,7 @@ class MainWindow(Tk):
         # Row 4
 
         # Row 5
-        Listbox(
+        ListBoxPlus(
             self, name='paper_shower', width=50, selectmode=MULTIPLE
         ).grid(row=5, column=1, rowspan=3)
 
@@ -64,6 +147,43 @@ class MainWindow(Tk):
             self, text='Download Selected', state=DISABLED, name='bt_download_selected',
             command=self.__download_selected_button_clicked
         ).grid(row=6, column=2)
+
+        # Row 7
+
+
+        # Row 8
+        Button(self, text='Enable Query', name='qry_enable',
+               command=lambda: (self.fill_qry(qry), self.children['qry_enable'].destroy())).grid(row=8, column=1)
+
+        # Row 9
+        qry = Frame(self, name='qry')
+
+        qry.grid(row=9, column=1)
+
+
+        # Row 9
+
+    def fill_qry(self, master):
+        def start_search():
+            self.children['paper_shower'].query(year=year_entry.get().split(','),
+                                                type=type_entry.get().split(','),
+                                                paper=paper_entry.get().split(','))
+
+
+        Label(master, text='Enter year (s18). Seperate with comma. DO NOT TYPE SPACE.').grid(column=1)
+        year_entry = Entry(master)
+        year_entry.grid(column=1)
+
+        Label(master, text='Enter type (qp, ms)').grid(column=1)
+        type_entry = Entry(master)
+        type_entry.grid(column=1)
+
+        Label(master, text='Enter paper (31)').grid(column=1)
+        paper_entry = Entry(master)
+        paper_entry.grid(column=1)
+
+        Button(text='start_search', command=start_search).grid(column=1)
+
 
     def __update_database_location(self):
         """
@@ -144,8 +264,14 @@ class MainWindow(Tk):
         download_papers(subject, code, to_path, self.__get_selected_papers())
 
 
+
+
+
+
+
 def main():
     app = MainWindow()
+
     app.mainloop()
 
 
